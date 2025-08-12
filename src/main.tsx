0d18f55d7695739c98fdca19b9f228a8a2ee6004
KRMGJ <dur369667@naver.com>
2025-08-12 18:11:33 +0900

    router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <CookiesProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+    </CookiesProvider>
+  );
+} else {
+  console.error("Root element not found");
+}
