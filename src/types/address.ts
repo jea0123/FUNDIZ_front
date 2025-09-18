7cbeaf11619a7b9472a945d39d655d75996399e6
LeeYG03 <LeeYG3673@gmail.com>
2025-09-18 21:14:01 +0900

    commit9_18

--- src/types/address.ts
+++ src/types/address.ts
@@ -7,5 +7,29 @@ export interface AddrAddRequest{
     roadAddr : string;
     detailAddr : string;
     recipientPhone : string;
-    isDefault : boolean;
+    isDefault : string;
+}
+
+export interface AddrUpdateRequest{
+    addrId :number;
+    userId : number;
+    addrName : string;
+    recipient : string;
+    postalCode : string;
+    roadAddr : string;
+    detailAddr : string;
+    recipientPhone : string;
+    isDefault : string;
+}
+
+export interface AddressResponse{
+    addrId : number;
+    userId : number;
+    addrName : string;
+    recipient : string;
+    postalCode : string;
+    roadAddr : string;
+    detailAddr : string;
+    recipientPhone : string;
+    isDefault : string;
 }
