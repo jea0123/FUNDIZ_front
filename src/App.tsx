973d117e4504da810abc20ad5e0d2d2413e48e55
KRMGJ <dur369667@naver.com>
2025-10-05 22:49:25 +0900

    feat(layout): add Header and Footer components to Layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { lazy, Suspense, useEffect } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom';
 import { LoginPage } from './views/auth/LoginPage';
 import { MyPage } from './views/user/MyPage';
-import { Navbar, Footer } from './components/Navbar';
+import { Footer } from './layout/Header';
 import { RegisterPage } from './views/auth/RegisterPage';
 import { useLoginUserStore } from './store/LoginUserStore.store';
 import { useCookies } from 'react-cookie';
@@ -17,7 +17,6 @@ import ProjectsAllPage, { ProjectByCategoryPage, ProjectBySubcategoryPage, Searc
 import { FundingPage } from './views/backing/backingPage';
 import FundingLoader from './components/FundingLoader';
 import { ApprovalDetail } from './views/admin/tabs/ApprovalDetail';
-import { AdminTabs } from './views/admin/AdminTabs';
 import AdminProjectEdit from './views/admin/tabs/AdminProjectEdit';
 import NotificationsPage from './components/NotificationsPage';
 import CreateProject from './views/creator/pages/CreateProject';
@@ -25,6 +24,9 @@ import CreatorDashboard from './views/creator/pages/CreatorDashboard';
 import CreatorProjects from './views/creator/pages/CreatorProjects';
 import CreatorLayout from './views/creator/CreatorLayout';
 import { AdminConsole } from './views/admin/AdminConsole';
+import Layout from './layout/Layout';
+
+const AdminTabs = lazy(() => import('./views/admin/AdminTabs').then(module => ({ default: module.AdminTabs })));
 
 export default function App() {
     const { setLoginUser, resetLoginUser } = useLoginUserStore();
@@ -54,61 +56,63 @@ export default function App() {
         <Suspense fallback={<FundingLoader />}>
             <div className="min-h-screen bg-gray-50">
                 <NavigatorRegistrar />
-                <Navbar />
+                {/* <Layout /> */}
                 <Routes>
-                    <Route path='/' element={<MainPage />} />
+                    <Route element={<Layout />}>
+                        <Route path='/' element={<MainPage />} />
 
-                    <Route path='/auth'>
-                        <Route path='register' element={<RegisterPage />} />
-                        <Route path='login' element={<LoginPage />} />
-                    </Route>
-
-                    <Route path='/project'>
-                        <Route index element={<ProjectsAllPage />} />
-                        <Route path='search' element={<SearchProjectPage />} />
-                        <Route path='category/:ctgrId' element={<ProjectByCategoryPage />} />
-                        <Route path='category/:ctgrId/subcategory/:subctgrId' element={<ProjectBySubcategoryPage />} />
-                        <Route path=':projectId' element={<ProjectDetailPage />} />
-                        <Route path=':projectId/backing' element={<FundingPage onBackClick={function (): void {
-                            throw new Error('Function not implemented.');
-                        }} onCompleteClick={function (): void {
-                            throw new Error('Function not implemented.');
-                        }} />} />
-                    </Route>
-
-                    <Route path='/user'>
-                        <Route path='Mypage' element={<MyPage />} />
-                    </Route>
-
-                    <Route path='/creator' element={<CreatorLayout />}>
-                        <Route index element={<CreatorDashboard />} />
-                        <Route path='dashboard' element={<CreatorDashboard />} />
-                        <Route path='project/new' element={<CreateProject />} />
-                        <Route path='projects'>
-                            <Route index element={<CreatorProjects />} />
-                            {/* TODO: 프로젝트 상세 */}
-                            {/* <Route path=':projectId' element={<CreatorProjectDetail />} /> */}
+                        <Route path='/auth'>
+                            <Route path='register' element={<RegisterPage />} />
+                            <Route path='login' element={<LoginPage />} />
                         </Route>
-                        <Route path='backings' />
-                        <Route path='shipping' />
-                        <Route path='qna' />
-                        <Route path='settlement' />
-                    </Route>
 
-                    <Route path='/admin' element={<AdminConsole />}>
-                        <Route index element={<AdminTabs />} />
-                        <Route path='verify/:projectId' element={<ApprovalDetail />} />
-                        <Route path='project/:projectId' element={<AdminProjectEdit />} />
-                    </Route>
+                        <Route path='/project'>
+                            <Route index element={<ProjectsAllPage />} />
+                            <Route path='search' element={<SearchProjectPage />} />
+                            <Route path='category/:ctgrId' element={<ProjectByCategoryPage />} />
+                            <Route path='category/:ctgrId/subcategory/:subctgrId' element={<ProjectBySubcategoryPage />} />
+                            <Route path=':projectId' element={<ProjectDetailPage />} />
+                            <Route path=':projectId/backing' element={<FundingPage onBackClick={function (): void {
+                                throw new Error('Function not implemented.');
+                            }} onCompleteClick={function (): void {
+                                throw new Error('Function not implemented.');
+                            }} />} />
+                        </Route>
 
-                    <Route path="/cs">
-                        <Route index element={<CSPage />} />
-                        <Route path='notice/:noticeId' element={<NoticeDetailPage />} />
-                    </Route>
+                        <Route path='/user'>
+                            <Route path='Mypage' element={<MyPage />} />
+                        </Route>
 
-                    <Route path="/error" element={<ErrorPage />} />
-                    <Route path="*" element={<ErrorPage />} />
-                    <Route path="/notifications" element={<NotificationsPage />} />/
+                        <Route path='/creator' element={<CreatorLayout />}>
+                            <Route index element={<CreatorDashboard />} />
+                            <Route path='dashboard' element={<CreatorDashboard />} />
+                            <Route path='project/new' element={<CreateProject />} />
+                            <Route path='projects'>
+                                <Route index element={<CreatorProjects />} />
+                                {/* TODO: 프로젝트 상세 */}
+                                {/* <Route path=':projectId' element={<CreatorProjectDetail />} /> */}
+                            </Route>
+                            <Route path='backings' />
+                            <Route path='shipping' />
+                            <Route path='qna' />
+                            <Route path='settlement' />
+                        </Route>
+
+                        <Route path='/admin' element={<AdminConsole />}>
+                            <Route index element={<AdminTabs />} />
+                            <Route path='verify/:projectId' element={<ApprovalDetail />} />
+                            <Route path='project/:projectId' element={<AdminProjectEdit />} />
+                        </Route>
+
+                        <Route path="/cs">
+                            <Route index element={<CSPage />} />
+                            <Route path='notice/:noticeId' element={<NoticeDetailPage />} />
+                        </Route>
+
+                        <Route path="/error" element={<ErrorPage />} />
+                        <Route path="*" element={<ErrorPage />} />
+                        <Route path="/notifications" element={<NotificationsPage />} />
+                    </Route>
                 </Routes>
                 <Footer />
             </div>
