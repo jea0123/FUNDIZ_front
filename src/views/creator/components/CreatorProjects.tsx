0c9afe90b562ae7063cd8fea13528d2cedef64e5
jea0123 <eunae8895@gmail.com>
2025-10-23 19:03:55 +0900

    refactor(ui): project detail page

--- src/views/creator/components/CreatorProjects.tsx
+++ src/views/creator/components/CreatorProjects.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Card, CardContent } from "@/components/ui/card";
+import { CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Loader2 } from "lucide-react";
 import { percent, toWon } from "@/utils/utils";
